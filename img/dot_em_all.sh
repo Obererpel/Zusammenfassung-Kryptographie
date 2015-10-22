92a21e89631ed5c5d489ba0bcb3168899b1b5b6c
Marc Meier <marceric.meier@gmail.com>
2015-10-22 22:50:17 +0200

    Mitschriften 22.10 + MaxFlow-Graph

--- img/dot_em_all.sh
+++ img/dot_em_all.sh
@@ -1,5 +1,6 @@
-
 neato clique1.dot -Tpng > clique1.png
 neato clique2.dot -Tpng > clique2.png
 neato clique3.dot -Tpng > clique3.png
 neato clique4.dot -Tpng > clique4.png
+
+dot max_flow.dot -Tpng > max_flow.png
