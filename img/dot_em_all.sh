c148d29905fa92ccb25505f699805d1527f8e681
Marc Meier <marceric.meier@gmail.com>
2015-10-23 00:23:34 +0200

    Mehrere Graphen geplottet

--- img/dot_em_all.sh
+++ img/dot_em_all.sh
@@ -4,3 +4,7 @@ neato clique3.dot -Tpng > clique3.png
 neato clique4.dot -Tpng > clique4.png
 
 dot max_flow.dot -Tpng > max_flow.png
+dot 3Sat_Ind_Set.dot -Tpng > 3Sat_Ind_Set.png
+dot 3Sat_3Col_single_clause.dot -Tpng > 3Sat_3Col_single_clause.png
+neato 3Sat_3Col_central.dot -Tpng > 3Sat_3Col_central.png
+dot 3Sat_3Col_example.dot -Tpng > 3Sat_3Col_example.png
