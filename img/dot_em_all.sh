6402967973980a6a668c337bb18fbff897bff17a
pj035 <pm.jaecks@gmail.com>
2015-10-25 11:09:13 +0100

    hoffentlich die Fehler bei den Bildern gefixt..

--- img/dot_em_all.sh
+++ img/dot_em_all.sh
@@ -2,6 +2,7 @@ neato clique1.dot -Tpng > clique1.png
 neato clique2.dot -Tpng > clique2.png
 neato clique3.dot -Tpng > clique3.png
 neato clique4.dot -Tpng > clique4.png
+
 dot max_flow.dot -Tpng > max_flow.png
 dot 3Sat_Ind_Set.dot -Tpng > 3Sat_Ind_Set.png
 dot 3Sat_3Col_single_clause.dot -Tpng > 3Sat_3Col_single_clause.png
